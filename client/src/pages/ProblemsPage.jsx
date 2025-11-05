9bf7ecfe809d97c4979db270ad40bd2a2c89fb70
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-11-05 23:22:10 +0530

    ProblemsPage Created

---
+++ client/src/pages/ProblemsPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProblemsPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Problems</h1>
+      <div className="bg-secondary p-6 rounded-lg shadow-md border border-border">
+        <p className="text-text-secondary">
+            This is where your list of DSA problme will go
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ProblemsPage
